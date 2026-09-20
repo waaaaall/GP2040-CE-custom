480edb7338daf58bfb2e75f65e4713aa387c4e39
waaaaall <nm7wall@gmail.com>
2026-09-20 18:33:05 +0900

    Fix nanopb pkg_resources compatibility and set ubuntu-22.04 runner

--- lib/nanopb/generator/proto/__init__.py
+++ lib/nanopb/generator/proto/__init__.py
@@ -7,7 +7,10 @@ import sys
 import tempfile
 import shutil
 import traceback
-import pkg_resources
+try:
+    import pkg_resources
+except ImportError:
+    pkg_resources = None
 from ._utils import has_grpcio_protoc, invoke_protoc, print_versions
 
 # Compatibility layer to make TemporaryDirectory() available on Python 2.
@@ -42,7 +45,11 @@ def build_nanopb_proto(protosrc, dirname):
     if has_grpcio_protoc():
         # grpcio-tools has an extra CLI argument
         # from grpc.tools.protoc __main__ invocation.
-        _builtin_proto_include = pkg_resources.resource_filename('grpc_tools', '_proto')
+        if pkg_resources is not None:
+            _builtin_proto_include = pkg_resources.resource_filename('grpc_tools', '_proto')
+        else:
+            import importlib.resources
+            _builtin_proto_include = str(importlib.resources.files('grpc_tools').joinpath('_proto'))
         cmd.append("-I={}".format(_builtin_proto_include))
 
     try:
